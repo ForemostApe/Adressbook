6abe3258e497f27638a035ce086d6755a246cabc
Per Karlsson <per.martin.karlsson@gmail.com>
2023-10-11 13:36:25 +0200

    Final commit.

--- Adressbook/FormAdressbook.Designer.cs
+++ Adressbook/FormAdressbook.Designer.cs
@@ -60,115 +60,115 @@
             // labelName
             // 
             labelName.AutoSize = true;
-            labelName.Location = new Point(16, 25);
-            labelName.Margin = new Padding(2, 0, 2, 0);
+            labelName.Location = new Point(30, 53);
+            labelName.Margin = new Padding(4, 0, 4, 0);
             labelName.Name = "labelName";
-            labelName.Size = new Size(40, 15);
+            labelName.Size = new Size(79, 32);
             labelName.TabIndex = 0;
             labelName.Text = "Namn";
             // 
             // labelStreet
             // 
             labelStreet.AutoSize = true;
-            labelStreet.Location = new Point(16, 75);
-            labelStreet.Margin = new Padding(2, 0, 2, 0);
+            labelStreet.Location = new Point(30, 160);
+            labelStreet.Margin = new Padding(4, 0, 4, 0);
             labelStreet.Name = "labelStreet";
-            labelStreet.Size = new Size(65, 15);
+            labelStreet.Size = new Size(131, 32);
             labelStreet.TabIndex = 1;
             labelStreet.Text = "Gatuadress";
             // 
             // labelPostalCode
             // 
             labelPostalCode.AutoSize = true;
-            labelPostalCode.Location = new Point(16, 125);
-            labelPostalCode.Margin = new Padding(2, 0, 2, 0);
+            labelPostalCode.Location = new Point(30, 267);
+            labelPostalCode.Margin = new Padding(4, 0, 4, 0);
             labelPostalCode.Name = "labelPostalCode";
-            labelPostalCode.Size = new Size(76, 15);
+            labelPostalCode.Size = new Size(149, 32);
             labelPostalCode.TabIndex = 2;
             labelPostalCode.Text = "Postnummer";
             // 
             // labelCity
             // 
             labelCity.AutoSize = true;
-            labelCity.Location = new Point(16, 175);
-            labelCity.Margin = new Padding(2, 0, 2, 0);
+            labelCity.Location = new Point(30, 373);
+            labelCity.Margin = new Padding(4, 0, 4, 0);
             labelCity.Name = "labelCity";
-            labelCity.Size = new Size(45, 15);
+            labelCity.Size = new Size(88, 32);
             labelCity.TabIndex = 3;
             labelCity.Text = "Postort";
             // 
             // labelPhoneNumber
             // 
             labelPhoneNumber.AutoSize = true;
-            labelPhoneNumber.Location = new Point(16, 225);
-            labelPhoneNumber.Margin = new Padding(2, 0, 2, 0);
+            labelPhoneNumber.Location = new Point(30, 480);
+            labelPhoneNumber.Margin = new Padding(4, 0, 4, 0);
             labelPhoneNumber.Name = "labelPhoneNumber";
-            labelPhoneNumber.Size = new Size(91, 15);
+            labelPhoneNumber.Size = new Size(184, 32);
             labelPhoneNumber.TabIndex = 4;
             labelPhoneNumber.Text = "Telefonnummer";
             // 
             // labelEmailAdress
             // 
             labelEmailAdress.AutoSize = true;
-            labelEmailAdress.Location = new Point(16, 275);
-            labelEmailAdress.Margin = new Padding(2, 0, 2, 0);
+            labelEmailAdress.Location = new Point(30, 587);
+            labelEmailAdress.Margin = new Padding(4, 0, 4, 0);
             labelEmailAdress.Name = "labelEmailAdress";
-            labelEmailAdress.Size = new Size(69, 15);
+            labelEmailAdress.Size = new Size(138, 32);
             labelEmailAdress.TabIndex = 5;
             labelEmailAdress.Text = "Emailadress";
             // 
             // textName
             // 
             textName.BorderStyle = BorderStyle.FixedSingle;
-            textName.Location = new Point(16, 41);
-            textName.Margin = new Padding(2, 1, 2, 1);
+            textName.Location = new Point(30, 87);
+            textName.Margin = new Padding(4, 2, 4, 2);
             textName.Name = "textName";
-            textName.Size = new Size(181, 23);
+            textName.Size = new Size(334, 39);
             textName.TabIndex = 10;
             // 
             // textStreet
             // 
             textStreet.BorderStyle = BorderStyle.FixedSingle;
-            textStreet.Location = new Point(16, 91);
-            textStreet.Margin = new Padding(2, 1, 2, 1);
+            textStreet.Location = new Point(30, 194);
+            textStreet.Margin = new Padding(4, 2, 4, 2);
             textStreet.Name = "textStreet";
-            textStreet.Size = new Size(181, 23);
+            textStreet.Size = new Size(334, 39);
             textStreet.TabIndex = 20;
             // 
             // textZipCode
             // 
             textZipCode.BorderStyle = BorderStyle.FixedSingle;
-            textZipCode.Location = new Point(16, 141);
-            textZipCode.Margin = new Padding(2, 1, 2, 1);
+            textZipCode.Location = new Point(30, 301);
+            textZipCode.Margin = new Padding(4, 2, 4, 2);
             textZipCode.Name = "textZipCode";
-            textZipCode.Size = new Size(179, 23);
+            textZipCode.Size = new Size(331, 39);
             textZipCode.TabIndex = 30;
             // 
             // textCity
             // 
             textCity.BorderStyle = BorderStyle.FixedSingle;
-            textCity.Location = new Point(16, 191);
-            textCity.Margin = new Padding(2, 1, 2, 1);
+            textCity.Location = new Point(30, 407);
+            textCity.Margin = new Padding(4, 2, 4, 2);
             textCity.Name = "textCity";
-            textCity.Size = new Size(181, 23);
+            textCity.Size = new Size(334, 39);
             textCity.TabIndex = 40;
             // 
             // textPhoneNumber
             // 
             textPhoneNumber.BorderStyle = BorderStyle.FixedSingle;
-            textPhoneNumber.Location = new Point(16, 241);
-            textPhoneNumber.Margin = new Padding(2, 1, 2, 1);
+            textPhoneNumber.Location = new Point(30, 514);
+            textPhoneNumber.Margin = new Padding(4, 2, 4, 2);
             textPhoneNumber.Name = "textPhoneNumber";
-            textPhoneNumber.Size = new Size(181, 23);
+            textPhoneNumber.Size = new Size(334, 39);
             textPhoneNumber.TabIndex = 50;
             // 
             // textEmailAdress
             // 
             textEmailAdress.BorderStyle = BorderStyle.FixedSingle;
-            textEmailAdress.Location = new Point(16, 291);
-            textEmailAdress.Margin = new Padding(2, 1, 2, 1);
+            textEmailAdress.Location = new Point(30, 621);
+            textEmailAdress.Margin = new Padding(4, 2, 4, 2);
             textEmailAdress.Name = "textEmailAdress";
-            textEmailAdress.Size = new Size(181, 23);
+            textEmailAdress.Size = new Size(334, 39);
             textEmailAdress.TabIndex = 60;
             // 
             // listAdressEntries
@@ -176,10 +176,10 @@
             listAdressEntries.BorderStyle = BorderStyle.FixedSingle;
             listAdressEntries.Columns.AddRange(new ColumnHeader[] { columnName, columnStreet, columnPostalCode, columnCity, columnPhoneNumber, columnEmailAdress });
             listAdressEntries.FullRowSelect = true;
-            listAdressEntries.Location = new Point(242, 115);
-            listAdressEntries.Margin = new Padding(2, 1, 2, 1);
+            listAdressEntries.Location = new Point(449, 245);
+            listAdressEntries.Margin = new Padding(4, 2, 4, 2);
             listAdressEntries.Name = "listAdressEntries";
-            listAdressEntries.Size = new Size(750, 223);
+            listAdressEntries.Size = new Size(1391, 473);
             listAdressEntries.TabIndex = 12;
             listAdressEntries.UseCompatibleStateImageBehavior = false;
             listAdressEntries.View = View.Details;
@@ -188,48 +188,48 @@
             // columnName
             // 
             columnName.Text = "Namn";
-            columnName.Width = 150;
+            columnName.Width = 260;
             // 
             // columnStreet
             // 
             columnStreet.Text = "Gatuadress";
-            columnStreet.Width = 150;
+            columnStreet.Width = 260;
             // 
             // columnPostalCode
             // 
             columnPostalCode.Text = "Postnummer";
-            columnPostalCode.Width = 100;
+            columnPostalCode.Width = 150;
             // 
             // columnCity
             // 
             columnCity.Text = "Postort";
-            columnCity.Width = 100;
+            columnCity.Width = 200;
             // 
             // columnPhoneNumber
             // 
             columnPhoneNumber.Text = "Telefonnummer";
-            columnPhoneNumber.Width = 100;
+            columnPhoneNumber.Width = 150;
             // 
             // columnEmailAdress
             // 
             columnEmailAdress.Text = "Emailadress";
-            columnEmailAdress.Width = 150;
+            columnEmailAdress.Width = 290;
             // 
             // textSearchBox
             // 
             textSearchBox.BorderStyle = BorderStyle.FixedSingle;
-            textSearchBox.Location = new Point(532, 65);
-            textSearchBox.Margin = new Padding(2, 1, 2, 1);
+            textSearchBox.Location = new Point(988, 139);
+            textSearchBox.Margin = new Padding(4, 2, 4, 2);
             textSearchBox.Name = "textSearchBox";
-            textSearchBox.Size = new Size(350, 23);
+            textSearchBox.Size = new Size(648, 39);
             textSearchBox.TabIndex = 100;
             // 
             // buttonSave
             // 
-            buttonSave.Location = new Point(117, 327);
-            buttonSave.Margin = new Padding(2, 1, 2, 1);
+            buttonSave.Location = new Point(217, 698);
+            buttonSave.Margin = new Padding(4, 2, 4, 2);
             buttonSave.Name = "buttonSave";
-            buttonSave.Size = new Size(81, 22);
+            buttonSave.Size = new Size(150, 47);
             buttonSave.TabIndex = 80;
             buttonSave.Text = "Spara";
             buttonSave.UseVisualStyleBackColor = true;
@@ -237,10 +237,10 @@
             // 
             // buttonSearch
             // 
-            buttonSearch.Location = new Point(911, 63);
-            buttonSearch.Margin = new Padding(2, 1, 2, 1);
+            buttonSearch.Location = new Point(1692, 134);
+            buttonSearch.Margin = new Padding(4, 2, 4, 2);
             buttonSearch.Name = "buttonSearch";
-            buttonSearch.Size = new Size(81, 23);
+            buttonSearch.Size = new Size(150, 49);
             buttonSearch.TabIndex = 110;
             buttonSearch.Text = "Sök";
             buttonSearch.UseVisualStyleBackColor = true;
@@ -248,10 +248,10 @@
             // 
             // buttonDelete
             // 
-            buttonDelete.Location = new Point(911, 352);
-            buttonDelete.Margin = new Padding(2, 1, 2, 1);
+            buttonDelete.Location = new Point(1692, 751);
+            buttonDelete.Margin = new Padding(4, 2, 4, 2);
             buttonDelete.Name = "buttonDelete";
-            buttonDelete.Size = new Size(81, 23);
+            buttonDelete.Size = new Size(150, 49);
             buttonDelete.TabIndex = 120;
             buttonDelete.Text = "Ta bort";
             buttonDelete.UseVisualStyleBackColor = true;
@@ -259,9 +259,10 @@
             // 
             // buttonModify
             // 
-            buttonModify.Location = new Point(117, 327);
+            buttonModify.Location = new Point(217, 698);
+            buttonModify.Margin = new Padding(6);
             buttonModify.Name = "buttonModify";
-            buttonModify.Size = new Size(81, 23);
+            buttonModify.Size = new Size(150, 49);
             buttonModify.TabIndex = 90;
             buttonModify.Text = "Ändra";
             buttonModify.UseVisualStyleBackColor = true;
@@ -270,9 +271,10 @@
             // 
             // buttonClear
             // 
-            buttonClear.Location = new Point(16, 327);
+            buttonClear.Location = new Point(30, 698);
+            buttonClear.Margin = new Padding(6);
             buttonClear.Name = "buttonClear";
-            buttonClear.Size = new Size(81, 23);
+            buttonClear.Size = new Size(150, 49);
             buttonClear.TabIndex = 70;
             buttonClear.Text = "Töm";
             buttonClear.UseVisualStyleBackColor = true;
@@ -295,24 +297,26 @@
             groupBox1.Controls.Add(textStreet);
             groupBox1.Controls.Add(textZipCode);
             groupBox1.Controls.Add(textCity);
-            groupBox1.Location = new Point(12, 24);
+            groupBox1.Location = new Point(22, 51);
+            groupBox1.Margin = new Padding(6);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(216, 370);
+            groupBox1.Padding = new Padding(6);
+            groupBox1.Size = new Size(401, 789);
             groupBox1.TabIndex = 19;
             groupBox1.TabStop = false;
             groupBox1.Text = "Adressuppgifter";
             // 
             // FormAdressbook
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1027, 416);
+            ClientSize = new Size(1907, 887);
             Controls.Add(groupBox1);
             Controls.Add(buttonDelete);
             Controls.Add(buttonSearch);
             Controls.Add(textSearchBox);
             Controls.Add(listAdressEntries);
-            Margin = new Padding(2, 1, 2, 1);
+            Margin = new Padding(4, 2, 4, 2);
             Name = "FormAdressbook";
             Text = "FormAdressbook";
             Load += FormAdressbook_Load;
